8b85e23871c16022fc3441c6f673c0ed00d92648
Musawar Hussain <hussainmusawar89@gmail.com>
2023-10-11 11:25:36 +0500

    product screen

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -9,6 +9,36 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Grapes',
+    'Banana',
+    'Cherry',
+    'Peach',
+    'Mixed Fruit Baskit',
+  ];
+  List<String> productUnit = [
+    'KG',
+    'KG',
+    'KG',
+    'Dozen',
+    'KG',
+    'KG',
+    'KG',
+  ];
+
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+  List<String> productImage = [
+    'https://images.unsplash.com/photo-1553279768-865429fa0078?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1547514701-42782101795e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8b3JhbmdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1596363505729-4190a9506133?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JhcGVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmFuYW5hfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1528821154947-1aa3d1b74941?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlcnJ5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1595124245030-41448b199d6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+    'https://media.istockphoto.com/id/1206376611/photo/tropical-fruits-assortment-on-a-plate-grey-background-close-up-top-view.webp?b=1&s=170667a&w=0&k=20&c=fspP2feq34_vMfPVxyyN9RWaCawTKYanh2atyv0Ao24=',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,15 +66,79 @@ class _ProductListScreenState extends State<ProductListScreen> {
         children: [
           Expanded(
               child: ListView.builder(
-            itemCount: 300,
+            itemCount: productName.length,
             itemBuilder: (context, index) {
               return Card(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [Text(index.toString())],
-                    )
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
+                        children: [
+                          Image(
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              productImage[index].toString(),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  productName[index].toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  productUnit[index].toString() +
+                                      " " +
+                                      r"$" +
+                                      productPrice[index].toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Container(
+                                    height: 35,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Add to cart',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               );
             },
